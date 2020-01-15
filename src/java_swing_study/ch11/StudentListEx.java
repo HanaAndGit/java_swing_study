ee239c762697e9fbcea6c4679feb0e05ca98728d
HanaAndGit <hanabiyou@naver.com>
2020-01-15 17:15:54 +0900

    J팝업메뉴 / 리스트에서 마우스 우클릭->수정, 삭제 메뉴 / 수정 : 리스트의 학생 객체가 텍스트 필드로 올라감 + 추가 버튼이 수정 버튼으로 변경 / 수정 클릭시 변경된 객체로 다시 리스트로 추가됨 / 삭제 : 클릭 시 리스트에서 삭제 (리스트에서 삭제는 x / array에서 삭제)

--- src/java_swing_study/ch11/StudentListEx.java
+++ src/java_swing_study/ch11/StudentListEx.java
@@ -1,6 +1,7 @@
 package java_swing_study.ch11;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +11,9 @@ import java.util.Vector;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
@@ -52,7 +55,12 @@ public class StudentListEx extends JFrame implements ActionListener {
 	 */
 	public StudentListEx() {
 		v = new  ArrayList<Student>();
+		v.add(new Student(1, "서현진", 80, 80, 90));
+		v.add(new Student(2, "이성경", 90, 10, 20));
+		v.add(new Student(3, "이유영", 80, 80, 20));
+		
 		initialize();
+		list.setListData(new Vector<>(v));
 	}
 	private void initialize() {
 		setTitle("학생관리");
@@ -88,6 +96,8 @@ public class StudentListEx extends JFrame implements ActionListener {
 		
 		pStd = new StudentPanel();
 		contentPane.add(pStd, BorderLayout.NORTH);
+		
+		list.setComponentPopupMenu(createPopupMenu());
 	}
 	
 	
@@ -99,15 +109,82 @@ public class StudentListEx extends JFrame implements ActionListener {
 		
 	}
 	
+	//마우스 우클릭 했을 때 팝업 메뉴 
+	private JPopupMenu createPopupMenu() {
+		JPopupMenu popMenu = new JPopupMenu();
+		
+		JMenuItem updateItem = new JMenuItem("수정");
+		updateItem.addActionListener(myPopMenuListener);
+		popMenu.add(updateItem);
+		
+		JMenuItem deleteItem = new JMenuItem("삭제");
+		deleteItem.addActionListener(myPopMenuListener);
+		popMenu.add(deleteItem);
+		
+		return popMenu;
+		
+	}
+	
+	ActionListener myPopMenuListener = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//actionCommand = 글자
+			if(e.getActionCommand().equals("수정")) {
+				//System.out.println("수정 ");
+				//Student std =
+				int selIdx = list.getSelectedIndex();
+				Student s = v.get(selIdx);
+				System.out.println(s);
+				
+				pStd.getTfStdNo().setText(Integer.toString(s.getStdNo()));
+				pStd.getTfName().setText(s.getStdName());
+				pStd.getTfKor().setText(Integer.toString(s.getKor()));
+				pStd.getTfMath().setText(Integer.toString(s.getMath()));
+				pStd.getTfEng().setText(Integer.toString(s.getEng()));
+				
+				btnAdd.setText("수정");
+				list.setListData(new Vector<>(v));
+				
+			}
+			if(e.getActionCommand().equals("삭제")) {
+				//System.out.println("삭제");
+				
+				//list에서 선택된 것이 있는지 판단
+				//선택된 인덱스 또는 value를 가져온 후에 
+				//arrayList에서 일치하는 것 삭제
+				//JList에서 setListData 호출 
+				
+				int selIdx = list.getSelectedIndex();
+				v.remove(selIdx);
+				list.setListData(new Vector<>(v));
+				
+			}
+				
+			
+		}
+	}; 
+	
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == btnAdd) {
 			btnAddActionPerformed(e);
 		}
 	}
 	protected void btnAddActionPerformed(ActionEvent e) {
-		Student std = pStd.getItem();
-		v.add(std);
-		list.setListData(new Vector<>(v));
+		
+		if(btnAdd.getText().equals("추가")) {
+			Student std = pStd.getItem();
+			v.add(std);
+			list.setListData(new Vector<>(v));
+			pStd.clearTf();
+		}else {
+			Student updateStd = pStd.getItem();
+			v.set(v.indexOf(updateStd), updateStd);
+			list.setListData(new Vector<>(v));
+			btnAdd.setText("추가");
+			pStd.clearTf();
+		}
+		
 		
 	}
 }
