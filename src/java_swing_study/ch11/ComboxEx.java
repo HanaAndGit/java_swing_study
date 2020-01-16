8c2a753006d5c61f1397ad598eb73e0ba53d28f6
HanaAndGit <hanabiyou@naver.com>
2020-01-16 15:22:10 +0900

    콤보박스에서 모델 사용하기 /제네릭 추가 -> setmodel(model) -> getModel() 메서드로 따로 빼기

---
+++ src/java_swing_study/ch11/ComboxEx.java
@@ -0,0 +1,96 @@
+package java_swing_study.ch11;
+
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+public class ComboxEx extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel pLeft;
+	private JPanel pRight;
+	private JComboBox<String> cmbFruit;
+	private JComboBox cmbName;
+	private DefaultComboBoxModel<String> model;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ComboxEx frame = new ComboxEx();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ComboxEx() {
+		initialize();
+	}
+
+	private void initialize() {
+		setTitle("JCombobox 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
+
+		pLeft = new JPanel();
+		pLeft.setBorder(
+				new TitledBorder(null, "\uC608\uC81C11-12", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(pLeft);
+
+		cmbFruit = new JComboBox<>();
+		cmbFruit.setMaximumRowCount(5);
+	
+		pLeft.add(cmbFruit);
+
+		cmbName = new JComboBox();
+		pLeft.add(cmbName);
+
+		pRight = new JPanel();
+		pRight.setBorder(
+				new TitledBorder(null, "\uC608\uC81C 11-13", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPane.add(pRight);
+		
+		//cmbFruits에 제네릭 추가 후 콤보박스모델에서 setmodel(model)로 변경
+		
+		
+		//cmbFruit.setModel(model);
+		cmbFruit.setModel(getModel());
+		cmbFruit.setSelectedIndex(-1);
+	}
+
+	private ComboBoxModel<String> getModel() {
+		
+		model = new DefaultComboBoxModel<String>();
+		
+		String[] fs = new String[] { "apple", "banana", "kiwi","mango", "pear", "peach", "berry", "strawberry", "blackberry" };
+		//List<String> fruits = new ArrayList<>(); //ArrayList의 부모는 List 
+		for(String f : fs) {
+			model.addElement(f);
+		}
+		return model;
+	}
+
+}
